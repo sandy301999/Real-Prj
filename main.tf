801738bc5756b7b76d96a5a628c90fb5b6843ea6
sandy301999 <61046327+sandy301999@users.noreply.github.com>
2025-09-09 17:48:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "ec2" {
   ami           = "ami-00ca32bbc84273381"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "${var.ec2_name[count.index]}-${count.index}"
-  }
+tags = {
+  Name = "${var.ec2_name[0]}-${count.index}"
+}
+
+
 }
